efaf8e58002a6101cef50340f41477a95ede090b
GWeightman <g.weightman@hotmail.co.uk>
2021-12-09 10:19:56 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const getpic = () => {
+    const response = fetch(`https://api.thecatapi.com/v1/images/search&api_key=7069bf1c-23bc-4ed2-925d-3c546afb9260`)
+    const data = response.json()
+    setProfile({pic: data[0].url})
+  }
+  
+  const [profile, setProfile] = useState([
+    {name: "Jordan", age: 6, price: "£200" , pic:{getpic}},
+    {name: "Wayne", age: 8, price: "£125", pic:{getpic}}, 
+    {name: "Gordon", age: 1, price: "£170", pic:{getpic}}, 
+    {name: "Jeff", age: 3, price: "£105", pic:{getpic}}, 
+    {name: "Simon", age: 9, price: "£180", pic:{getpic}},
+  ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='display'>
+      <h1>Cat Shop</h1>
+      {profile.map((profile) => {
+        return(
+          <div className="card">
+            <img src={profile.pic} alt='no pic'/>
+            <p>{profile.name}</p>
+            <p>{profile.age}</p>
+            <p>{profile.price}</p>
+            <button>+</button>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
+  
 
 export default App;
